ea792f38eab640c1784ed44d2e2a3ad20aa2bc77
Jenia Isler <jenia.isler@gmx.ch>
2021-01-20 16:01:03 +0100

    changed indents

--- poc/client_server_socket/MyWorld.java
+++ poc/client_server_socket/MyWorld.java
@@ -14,68 +14,60 @@ public class MyWorld extends World
      * 
      */
     private boolean first = true;
-    
+
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-            
-    
+
     }
-    
     public void act(){
-        
         if(first){
-            
             if(ActasServer()){
                 startServer();
             }else{
                 startClient();
             }
-            
+
             first = false;
         }
-        
+
     }
-    
+
     public void startServer(){
         try{
-            
-        Server.ServerSocket()/*SocketGetJSON()*/;
-    }  catch(Exception e){
-        e.printStackTrace();
-        System.out.println(e);
+
+            Server.ServerSocket()/*SocketGetJSON()*/;
+        }  catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
     }
-    }
-    
+
     public void startClient(){
         try{
-            
-        Client.ServerSocket();
-    }  catch(Exception e){
-        e.printStackTrace();
-        System.out.println(e);
+
+            Client.ServerSocket();
+        }  catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
     }
-    }
-    
-    
+
     
     public boolean ActasServer(){
-        
-        String response = null;
-        
-        do{
-            
-            response = Greenfoot.ask("Are you a Host [yes/no]");
-            
-        } while(!(response.contains("yes") || response.contains("no")));
-        
-    
-        if(response.contains("yes")){
 
+        String response = null;
+
+        do{
+
+            response = Greenfoot.ask("Are you a Host [yes/no]");
+
+        } while(!(response.contains("yes") || response.contains("no")));
+
+        if(response.contains("yes")){
             return true;
         }else{return false;}
     }
 }
-
 
